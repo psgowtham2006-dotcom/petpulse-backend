c069515208b9f88ceb3c6c5dcb2b75562bbc2901
psgowtham2006-dotcom <psgowtham2006@gmail.com>
2026-07-08 12:23:20 +0530

    add CORS

--- src/main/java/com/petpulse/security/WebSecurityConfig.java
+++ src/main/java/com/petpulse/security/WebSecurityConfig.java
@@ -92,7 +92,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5500", "http://localhost:5000", "http://localhost:8000", "http://127.0.0.1:8000","*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5500", "http://localhost:5000", "http://localhost:8000", "http://127.0.0.1:8000","https://psgowtham2006-dotcom.github.io/petpluse-frontend/", "*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
